01a59952446155e6289917402170d6d9ef4e0617
Abhilasha <pabhilashapatel09@gmail.com>
2024-07-24 17:32:16 +0530

    first commit

---
+++ src/main/java/com/BikeRentalProject/BikeRentalProject/BikeRentalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BikeRentalProject.BikeRentalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRentalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikeRentalProject/BikeRentalProject/BikeRentalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikeRentalProject.BikeRentalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
